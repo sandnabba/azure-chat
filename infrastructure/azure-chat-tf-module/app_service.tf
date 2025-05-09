4ec8b5713b08a4c570f3d6226750906c4dc66a15
Emil Sandnabba <emil@sandnabba.se>
2025-05-09 19:16:23 +0200

    Removing frontend app service, using blob storage instead

--- infrastructure/azure-chat-tf-module/app_service.tf
+++ infrastructure/azure-chat-tf-module/app_service.tf
@@ -43,9 +43,13 @@ resource "azurerm_linux_web_app" "backend" {
   location            = var.location
   resource_group_name = var.resource_group_name
   service_plan_id     = azurerm_service_plan.chat_app_service_plan.id
+  https_only          = true
 
   site_config {
     always_on        = true
+    ftps_state       = "Disabled"
+    minimum_tls_version = "1.2"
+    websockets_enabled = true
     app_command_line = "gunicorn src.app:app --bind 0.0.0.0:8000 --worker-class uvicorn.workers.UvicornWorker --timeout 120 --log-level info --access-logfile - --error-logfile -"
 
     application_stack {
@@ -117,59 +121,4 @@ resource "azurerm_linux_web_app" "backend" {
   }
 
   tags = var.tags
-}
-
-# Frontend App Service
-resource "azurerm_linux_web_app" "frontend" {
-  name                = var.frontend_app_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  service_plan_id     = azurerm_service_plan.chat_app_service_plan.id
-
-  site_config {
-    always_on = true
-
-    application_stack {
-      docker_image_name        = "${var.container_registry_login_server}/${var.frontend_image_name}:${var.frontend_image_tag}"
-      docker_registry_url      = "https://${var.container_registry_login_server}"
-      docker_registry_username = var.container_registry_admin_username
-      docker_registry_password = var.container_registry_admin_password
-    }
-  }
-
-  app_settings = {
-    "WEBSITES_PORT"                         = "80"
-    "WEBSITES_ENABLE_APP_SERVICE_STORAGE"   = "false"
-    "APPINSIGHTS_INSTRUMENTATIONKEY"        = azurerm_application_insights.chat_app_insights.instrumentation_key
-    "APPLICATIONINSIGHTS_CONNECTION_STRING" = azurerm_application_insights.chat_app_insights.connection_string
-  }
-
-  logs {
-    detailed_error_messages = true
-    failed_request_tracing  = true
-
-    application_logs {
-      file_system_level = "Information"
-    }
-
-    http_logs {
-      file_system {
-        retention_in_days = 7
-        retention_in_mb   = 100
-      }
-    }
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  lifecycle {
-    ignore_changes = [
-      tags["Cost Center"],
-      tags["hidden-link: /app-insights-conn-string"],
-      tags["hidden-link: /app-insights-instrumentation-key"],
-      tags["hidden-link: /app-insights-resource-id"]
-    ]
-  }
 }
